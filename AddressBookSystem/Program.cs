fdbf164217b397d57c99a510f792badf4374ac1f
Aj007ar <aj007.ar@gmail.com>
2022-08-17 23:14:01 +0530

    [Ajay] added: Create Contact Program

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\n****************WELCOME TO ADDRESS BOOK SYSTEM*************");
+            Console.WriteLine("1. Create Contact in address book:");
+            Console.WriteLine("Choose option for execute a program");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    UC1_CreateContact createContact = new UC1_CreateContact();
+                    createContact.Add_Contact();
+                    break;
+            }
         }
     }
 }
---
+++ AddressBookSystem/UC1_CreateContact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class UC1_CreateContact
+    {       
+           Dictionary<string, string> contact = new Dictionary<string, string>();
+
+            public void Add_Contact()
+            {
+                Console.WriteLine("Enter First Name:");
+                contact.Add("First_Name",Console.ReadLine());
+
+                Console.WriteLine("Enter Last Name:");
+                contact.Add("Last_Name", Console.ReadLine());
+
+                Console.WriteLine("Enter Address:");
+                contact.Add("Address", Console.ReadLine());
+
+                Console.WriteLine("Enter City:");
+                contact.Add("City_Name", Console.ReadLine());
+
+                Console.WriteLine("Enter State:");
+                contact.Add("State_Name", Console.ReadLine());
+
+                Console.WriteLine("Enter Zip code:");
+                contact.Add("Zip_Code", Console.ReadLine());
+
+                Console.WriteLine("Enter phone Number:");
+                contact.Add("Phone_Number", Console.ReadLine());
+
+                Console.WriteLine("Enter Email:");
+                contact.Add("Email", Console.ReadLine());
+            }       
+    }
+}
